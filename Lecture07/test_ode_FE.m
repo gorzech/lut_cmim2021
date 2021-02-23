03741ec8d2e52ecfadafb5a9c3b6f5760d4b9f52
GOrzech <2590699+gorzech@users.noreply.github.com>
2021-02-23 15:40:41 +0200

    Add two extra hand-ons from the classes.

--- Lecture07/test_ode_FE.m
+++ Lecture07/test_ode_FE.m
@@ -5,7 +5,7 @@ U_0 = 1;
 T = 3*dt;
 expected = [U_0, 2, 4, 8]';
 expected = [expected expected];
-expexted_time = (0:dt:T)';
+expected_time = (0:dt:T)';
 tol = eps(1e2);
 
 [time, sol] = ode_FE(f, [dt, T], [U_0; U_0]);
@@ -13,7 +13,7 @@ assert(isequal(size(sol), size(expected)), "Unexpected solution vector size [%s]
     num2str(size(sol)), num2str(size(expected)))
 assert(isequal([size(sol, 1), 1], size(time)), "Solution and time sized does not match")
 assert(max(max(abs(sol - expected))) < tol, "Forward Euler solution is inaccurate")
-assert(max(abs(time - expexted_time)) < tol, "Forward Euler time vector is incorrect")
+assert(max(abs(time - expected_time)) < tol, "Forward Euler time vector is incorrect")
 
 %% Second ode_FE test - population growth compared with known exact solution
 r1 = 0.1234;
@@ -23,7 +23,7 @@ U_0 = 5.64;
 n = 46;
 T = n * dt;
 f = @(t, u) [r1; r2] .* u;
-expexted_time = (0:dt:T)';
+expected_time = (0:dt:T)';
 expected = [U_0 * (1 + r1 * dt).^(0:n)'  U_0 * (1 + r2 * dt).^(0:n)'];
 tol = eps()*1e5;
 
@@ -32,5 +32,25 @@ assert(isequal(size(sol), size(expected)), "Unexpected solution vector size [%s]
     num2str(size(sol)), num2str(size(expected)))
 assert(isequal([size(sol, 1), 1], size(time)), "Solution and time sized does not match")
 assert(max(max(abs(sol - expected))) < tol, "Forward Euler solution is inaccurate")
-assert(max(abs(time - expexted_time)) < tol, "Forward Euler time vector is incorrect")
+assert(max(abs(time - expected_time)) < tol, "Forward Euler time vector is incorrect")
 
+%% Third ode_FE test - linear solution
+% test parameters
+% solution is linear in time: a * t + b
+clear
+a = 178.32;
+b = 1263461.4167;
+k = 46.5215;
+m = 4;
+dt = 0.1;
+T = 3.2;
+
+f = @(t, u) a + k * (u - (a * t + b)) .^ m;
+[time, sol] = ode_FE(f, [dt, T], b);
+
+expected_time = (0:dt:T)';
+expected_sol = a * expected_time + b;
+
+tol = eps(1e3 * b);
+assert(max(max(abs(sol - expected_sol))) < tol, "Forward Euler solution is inaccurate")
+assert(max(abs(time - expected_time)) < tol, "Forward Euler time vector is incorrect")
